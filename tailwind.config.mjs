296c080447e8502778a3c14e2357025938109093
Nazrul Islam <nazrulislambhat@gmail.com>
2025-02-18 16:03:43 +0530

    +

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        accent: 'var(--accent-color)',
+        background: 'var(--background-color)',
+        text: 'var(--text-color)',
+        button: 'var(--button-color)',
+      },
+      fontSize: {
+        h1: '36px', // For main titles or important headings
+        h2: '28px', // For subheadings and section titles
+        h3: '22px', // For smaller headings or section subtitles
+        body: '16px', // Ideal for paragraphs and general content
+        button: '14px', // Easy-to-read size for buttons or call-to-actions
       },
     },
   },
