ba45e2ca4bb0f46955b938563399a8106c1f713b
Nazrul Islam <nazrulislambhat@gmail.com>
2025-02-21 09:53:31 +0530

    first comit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: '#ffffff',
+        primary: '#011ef5',
+        secondary: '#cbff8c',
+        tomato: '#fb4d3d',
+        yellow: '#fffd82',
+        dark: '#011936',
       },
     },
   },
